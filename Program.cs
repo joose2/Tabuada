f7d128d47ea96eb719007187544003663d640363
jose <jose.silva2000@etec.sp.gov.br>
2022-11-21 20:37:50 -0300

    atividade da tabuada

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("OLÁ VAMOS FAZER ");
+Console.Write("Tabuada do número: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int m = 0;
+int produto;
+
+while (m <= 10)
+{
+
+        produto = n * m;
+    Console.WriteLine($"{n} x {m} = {produto}");
+    m += 1;
+
+}
